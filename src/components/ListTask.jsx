f4f70cb5dd0511d0d51f24571b68df8db2f59434
Arafat Patwary <farabipsk@gmail.com>
2024-11-13 10:54:07 +0600

    add new file

--- src/components/ListTask.jsx
+++ src/components/ListTask.jsx
@@ -1,10 +1,25 @@
 import { Stack } from "react-bootstrap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const ListTask = ({ tasks, setTasks }) => {
-    const [todos,setTodos] = useState([]);
-    const [inProgress,setInProgress] = useState([]);
-    const [closed,setClosed] = useState([]);
+    const [todos, setTodos] = useState([]);
+    const [inProgress, setInProgress] = useState([]);
+    const [closed, setClosed] = useState([]);
+
+    useEffect(() =>{
+      const fTodos = tasks.filter(task => task.status === "todos");
+      const fProgress = tasks.filter(task => task.status === "progress");
+      const fClosed = tasks.filter(task => task.status === "closed");
+
+      setTodos(fTodos);
+      setInProgress(fProgress);
+      setClosed(fClosed);
+    },[tasks]);
+
+    const statuses = ["todos", "inprogress", "closed"];
+
+   
+
   return (
     <div>
       List
